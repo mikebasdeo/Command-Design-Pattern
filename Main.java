4cd936e3367cf125659aeb01dac03d36547001f9
mikebasdeo <mikebasdeo@gmail.com>
2018-03-01 11:35:22 -0500

    Added comments to main.

--- Main.java
+++ Main.java
@@ -6,12 +6,16 @@ public class Main {
 
     public static void main(String args[]){
 
+        //Create a new ElectronicDevice that is a Television
         ElectronicDevice newDevice = TVRemote.getDevice();
 
+        //Create a command object that is connected to the TV.
         TurnTVOn onCommand = new TurnTVOn(newDevice);
 
+        //Create a button that is connected to the command.
         DeviceButton onPressed = new DeviceButton(onCommand);
 
+        //Press the button.
         onPressed.press();
 
     }
