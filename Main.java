f42b69020c7cfd27f4096b890b463990e8b9d60a
mikebasdeo <mikebasdeo@gmail.com>
2018-03-01 10:38:25 -0500

    Initial Commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.mike;
+
+
+/**
+ * Command Design Pattern
+ *
+* */
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("test");
+    }
+}
